7b5b61b33993cf7e11781908cc7e73b3ed4b8967
chenjishi <jishi@staff.xianguo.com>
2014-11-05 19:26:37 +0800

    first commit

---
+++ src/com/chenjishi/slideupdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.chenjishi.slideupdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        final float density = getResources().getDisplayMetrics().density;
+
+        SlidingUpPaneLayout slidingUpPaneLayout = (SlidingUpPaneLayout) findViewById(R.id.sliding_up_layout);
+        slidingUpPaneLayout.setParallaxDistance((int) (200 * density));
+        slidingUpPaneLayout.setShadowResourceTop(R.drawable.shadow_top);
+
+        slidingUpPaneLayout.openPane();
+    }
+}
